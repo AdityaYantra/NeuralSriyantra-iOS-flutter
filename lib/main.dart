67bda5e1c20ff084575ba8363b054463ad4f5507
AdityaYantra <aditya@sriyantra.ai>
2025-08-20 19:30:14 -0500

    Initialize Hive and add FlutterBluePlus service

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,16 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 import 'app.dart';
 import 'core/env.dart';
+import 'data/models/user_profile.dart';
 import 'services/firebase_initializer.dart';
 
-void main() async {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Hive.initFlutter();
+  Hive.registerAdapter(UserProfileAdapter());
   await Env.load();
   await FirebaseInitializer.initialize();
   runApp(const ProviderScope(child: App()));
